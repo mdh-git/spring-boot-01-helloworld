4028b985f9329fa922df9eb68ead79dec083b651
mdh-lcb <752228071@qq.com>
2019-01-15 17:24:49 +0800

    springboot 基础

---
+++ src/main/java/com/mdh/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.mdh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: madonghao
+ * @Date: 2019/1/15 11:57
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping(value = "hello")
+    public String hello(){
+        return "hello word!";
+    }
+}
